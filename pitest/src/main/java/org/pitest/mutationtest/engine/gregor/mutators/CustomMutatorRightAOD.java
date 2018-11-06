114ea07c95fe4c8a9b2f9ca3645a3763a5a5fb03
Barrett <Barrett@192.168.2.6>
2018-11-06 17:04:48 -0600

    changed mutator AOD name to better describe which section (Right) it focuses on

---
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/CustomMutatorRightAOD.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018 org.pitest.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pitest.mutationtest.engine.gregor.mutators;
+
+/**
+ *
+ * @author Barrett
+ */
+import org.objectweb.asm.MethodVisitor;
+import org.pitest.bytecode.ASMVersion;
+import org.pitest.mutationtest.engine.MutationIdentifier;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+import org.objectweb.asm.Opcodes;
+
+public enum CustomMutatorRightAOD implements MethodMutatorFactory {
+
+    CUSTOM_MUTATOR_AOD;
+
+    @Override
+    public MethodVisitor create(final MutationContext context,
+            final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+        return new IncrementsMethodVisitor(this, context, methodVisitor);
+    }
+
+    @Override
+    public String getGloballyUniqueId() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+}
+
+class CustomAODMethodVisitor extends MethodVisitor {
+
+    private final MethodMutatorFactory factory;
+    private final MutationContext context;
+
+    CustomAODMethodVisitor(final MethodMutatorFactory factory,
+            final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+        super(ASMVersion.ASM_VERSION, delegateMethodVisitor);
+        this.factory = factory;
+        this.context = context;
+    }
+
+    @Override
+    public void visitInsn(final int opcode) {
+        if ((opcode == Opcodes.IADD) || (opcode == Opcodes.ISUB))  {
+            final MutationIdentifier newId = this.context.registerMutation(this.factory, "->(AOD) KILLED");
+            if (this.context.shouldMutate(newId)) {
+                super.visitInsn(Opcodes.SWAP);
+                super.visitInsn(Opcodes.POP);
+            } else {
+                super.visitInsn(opcode);
+            } 
+        } else {
+           super.visitInsn(opcode);
+        }
+        //still have to do IADD
+
+    }
+
+}
